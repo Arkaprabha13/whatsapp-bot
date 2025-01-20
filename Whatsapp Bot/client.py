666027a97740d986e642f41e07aa857b3ee29165
Priyanshu Das ✍(◔◡◔) <priyanshudas030@gmail.com>
2025-01-20 21:34:29 +0530

    Update client.py
    changed api_key

--- Whatsapp Bot/client.py
+++ Whatsapp Bot/client.py
@@ -1,22 +1,18 @@
 from groq import Groq
 
 client = Groq(
-    api_key="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"
+    api_key="<your_api_key>"
 )
 
 command = '''
-[2:56 pm, 19/1/2025] </ThePriyanshuDas/>: Chalu hua session?
-[3:04 pm, 19/1/2025] Harsh (Roommate): Haa
-[12:00 pm, 20/1/2025] </ThePriyanshuDas/>: Vikas choudhary khud phone chala rhe hai
-[7:19 pm, 20/1/2025] </ThePriyanshuDas/>: hello
-[7:52 pm, 20/1/2025] Harsh (Roommate): Hello
+<test command to check that it produce response>
 '''
 completion = client.chat.completions.create(
     model="llama3-70b-8192",
     messages=[
         {
             "role": "system",
-            "content": "You are person named priyanshu who speaks both english and hindi. You are a creepy guy and flirt with bouth men and women. You are from India and you are a coder. You Analyze the chat history and resond like priyanshu. Output should be next chat response (text message only)"
+            "content": "You are person named priyanshu who speaks both english and hindi. You are from India and you are a coder. You Analyze the chat history and resond like priyanshu. Output should be next chat response (text message only)"
         },
         {
             "role": "user",
